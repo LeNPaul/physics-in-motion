0a14b7807a74653ab6e2c8d99b50f039854e7922
Paul Le <l.nguyen.paul@gmail.com>
2018-02-15 23:17:36 -0500

    Add code to render LaTeX when loading new views

--- public/js/controllers/NerdCtrl.js
+++ public/js/controllers/NerdCtrl.js
@@ -2,4 +2,10 @@ angular.module('NerdCtrl', []).controller('NerdController', function($scope) {
 
 	$scope.tagline = 'Nothing beats a pocket protector!';
 
+	// Render LaTex when loading new view
+	$scope.$watch(function(){
+	  MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+	  return true;
+	});
+
 });
