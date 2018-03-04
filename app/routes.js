e0b59e567f4544d545cd03d24f1d2d485617463b
Paul Le <l.nguyen.paul@gmail.com>
2018-03-04 00:04:26 -0500

    Update routes to include signup API

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,15 @@ module.exports = function(app) {
 	// handle things like api calls
 	// authentication routes
 
+	app.post('/signup', function(req, res) {
+
+		// Read in credentials
+		var email = req.body.email;
+		var password = req.body.password;
+		var password2 = req.body.password;
+
+	});
+
 	// frontend routes =========================================================
 	// route to handle all angular requests
 	app.get('/', function(req, res) {
