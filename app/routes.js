8ef9535d93816c6b1992a1644eee31bfdd3c59e2
Paul Le <l.nguyen.paul@gmail.com>
2018-03-10 21:55:11 -0500

    Add API endpoints for signin and signup

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,33 @@ module.exports = function(app) {
 	// handle things like api calls
 	// authentication routes
 
+	// Sample API route
+	// Grab the nerd model we just created
+	var Nerd = require('./models/nerd');
+	// Sample api route
+	app.get('/api/nerds', function(req, res) {
+			// Use mongoose to get all nerds in the database
+			Nerd.find(function(err, nerds) {
+
+					// If there is an error retrieving, send the error.
+													// Nothing after res.send(err) will execute
+					if (err)
+							res.send(err);
+
+					res.json(nerds); // Return all nerds in JSON format
+			});
+	});
+
+	app.post('/signin', function(req, res) {
+
+		// Read in credentials
+		var email = req.body.email;
+		var password = req.body.password;
+
+		console.log(email);
+
+	});
+
 	app.post('/signup', function(req, res) {
 
 		// Read in credentials
