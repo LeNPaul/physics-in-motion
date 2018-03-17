09316ff965558e33ea26e4cfe0b63ef57534a942
Paul Le <l.nguyen.paul@gmail.com>
2018-03-17 13:45:32 -0400

    Update server.js code

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// modules =================================================
+// Modules =================================================
 var express        = require('express');
 var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
@@ -17,7 +17,8 @@ var app            = express();
 // Config files
 var db = require('./config/db');
 
-var port = process.env.PORT || 8080; // set our port
+// Set our port
+var port = process.env.PORT || 8080;
 
 // MongoDB =================================================
 
@@ -34,16 +35,37 @@ mongoose.connection.on('disconnected', function() {
   console.log('Mongoose disconnected');
 });
 
+// Add HTTP request logging
+app.use(logger('dev'));
+
 // Get all data/stuff of the body (POST) parameters
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+
+// Establish cookie session
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'));
 
 // Routes ==================================================
 require('./app/routes')(app); // pass our application into our routes
+
+// Passport config
+var Account = require('./app/models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // Start application =======================================
 app.listen(port);
