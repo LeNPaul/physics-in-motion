5a29b2a84da1aa14d1fcc3cced50a738c5bff2a2
Paul Le <l.nguyen.paul@gmail.com>
2018-03-23 11:42:28 -0400

    Update AngularJS routing on the user page

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,39 @@
-var app = angular.module("myApp", ["ngRoute"]);
-app.config(function($routeProvider) {
-   $routeProvider
-    .when("/banana", {
-        template : "<h1>Banana</h1><p>Bananas contain around 75% water.</p>"
+// create the module and name it scotchApp
+var scotchApp = angular.module('scotchApp', ['ngRoute']);
+
+// configure our routes
+scotchApp.config(function($routeProvider) {
+  $routeProvider
+
+    // route for the home page
+    .when('/', {
+      templateUrl : 'pages/home.html',
+      controller  : 'mainController'
     })
-    .when("/tomato", {
-        template : "<h1>Tomato</h1><p>Tomatoes contain around 95% water.</p>"
+
+    // route for the about page
+    .when('/about', {
+      templateUrl : 'pages/about.html',
+      controller  : 'aboutController'
     })
-    .otherwise({
-        template : "<h1>None</h1><p>Nothing has been selected</p>"
+
+    // route for the contact page
+    .when('/contact', {
+      templateUrl : 'pages/contact.html',
+      controller  : 'contactController'
     });
+});
+
+// create the controller and inject Angular's $scope
+scotchApp.controller('mainController', function($scope) {
+  // create a message to display in our view
+  $scope.message = 'Everyone come and see how good I look!';
+});
+
+scotchApp.controller('aboutController', function($scope) {
+  $scope.message = 'Look! I am an about page.';
+});
+
+scotchApp.controller('contactController', function($scope) {
+  $scope.message = 'Contact us! JK. This is just a demo.';
 });
