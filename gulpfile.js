cda363c2b2b22b12d67dbd1ace3f5bdb82d14c63
Paul Le <l.nguyen.paul@gmail.com>
2018-04-12 11:20:48 -0400

    Update package.json and Gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
 
 gulp.task('hello-world', function(){
     console.log('hello world');
