95b207fa960bbc74b836b4129e12eebcee7da6a0
Paul Le <l.nguyen.paul@gmail.com>
2018-04-14 16:28:18 -0400

    Add nodemon and browser-sync tasks to gulpfile.js and update package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 var gulp = require('gulp');
+var gulpSequence = require('gulp-sequence');
 var browserSync = require('browser-sync');
 var nodemon = require('gulp-nodemon');
 
-gulp.task('hello-world', function(){
-    console.log('hello world');
+gulp.task('nodemon', function () {
+  nodemon({
+    script: './bin/www'
+  });
+});
+
+gulp.task('browser-sync', function () {
+  browserSync({
+    proxy: 'localhost:8080',
+    files: ['public/*'],
+    notify: false
+  });
 });
