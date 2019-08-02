7cd624a9a3d529e2d4fea102e93c0ae28f954341
Paul Le <l.nguyen.paul@gmail.com>
2019-08-01 20:39:13 -0400

    Add browser-sync command line configuration example

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ gulp.task('nodemon', function (done) {
   done();
 });
 
+//  Recreate this configuration
+//  browser-sync start --proxy localhost:8080 --files="public/css/*.css", "public/js/*.js" "public/views/*.pug"  --no-notify
 gulp.task('browser-sync', function (done) {
   browserSync({
     proxy: 'localhost:8080',
