3e2853fc396bdbb1349c72f029b42da1130b3e50
Paul Le <l.nguyen.paul@gmail.com>
2019-08-03 13:20:05 -0400

    Update lessons schema to include default note and date values #13 #14 #15

--- models/lessons.js
+++ models/lessons.js
@@ -14,145 +14,145 @@ const Lessons = new Schema({
       kinematics: {
         motion_in_one_dimension: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         motion_in_two_dimensions: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         simple_motion_in_one_dimension: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         simple_motion_in_two_dimensions: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         }
       },
       forces: {
         friction_drag: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         newtons_law: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         simple_forces: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         }
       },
       energy: {
         conservative_forces: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         energy_conservation_work: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         power: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         work_potential_energy: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         }
       },
       momentum: {
         elastic_collisions: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         explosions: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         momentum_conservation: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         }
       },
       simple_harmonic_motion: {
         damped_harmonic_motion: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         driven_oscillations: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         dynamics_simple_harmonic_motion: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         the_pendulum: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         }
       },
       waves: {
         characteristics_waves: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         interference: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         superposition_of_waves: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         }
       },
       fluids: {
         buoyancy: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         continuity: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         fluid_dynamics: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         incompressible_fluids: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         },
         pressure: {
           status: { type: Boolean, default: false },
-          updated: Date,
-          notes: String
+          updated: { type: Date, default: new Date() },
+          notes: { type: String, default: "" }
         }
       }
     }
