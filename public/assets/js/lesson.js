c40073258bf857c0fbdd2819a8b3f5c1df299e37
Paul Le <l.nguyen.paul@gmail.com>
2019-08-07 00:00:39 -0400

    Move functions for handling user lesson module information to separate module and refactor Kinematics lessons to use those functions #31

---
+++ public/assets/js/lesson.js
@@ -0,0 +1,48 @@
+var lessonData = document.querySelector("#lesson-module");
+var topic = lessonData.dataset.topic;
+var lesson = lessonData.dataset.lesson;
+
+var lessonPath = topic + "." + lesson;
+console.log(lessonPath);
+var username = document.head.querySelector("[name~=username][content]").content;
+
+function updateTime() {
+  $.post("/update_time", {username: username, lessonPath: lessonPath}, function(data, status) {
+    // Just update the last seen time
+    // TODO - add proper error handling here
+  });
+};
+
+function setSavedNote() {
+  $.post("/user_lessons", {username: username}, function(data, status) {
+    document.getElementById('lessonNotes').value = data[0]["lesson_modules"][topic][lesson].notes;
+  })
+}
+
+function updateNotes() {
+  var notes = document.getElementById("lessonNotes").value;
+  $.post("/update_notes", {username: username, lessonPath: lessonPath, notes: notes}, function(data, status) {
+  });
+}
+
+function setLessonStatus() {
+  $.post("/user_lessons", {username: username}, function(data, status) {
+    if(status) {
+      if(data[0]["lesson_modules"][topic][lesson].status) {
+        document.getElementById("lessonStatus").textContent="Complete!";
+      } else {
+        document.getElementById("lessonStatus").textContent="Incomplete!";
+      }
+    }
+  });
+}
+
+function updateLessonStatus(status) {
+  $.post("/update_lesson_status", {username: username, lessonPath: lessonPath, status: status}, function(data, status) {
+    setLessonStatus()
+  });
+}
+
+updateTime();
+setLessonStatus();
+setSavedNote();
