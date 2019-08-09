f393a5aa2088cbafd431fb73b33a0f0479a9c02e
Paul Le <l.nguyen.paul@gmail.com>
2019-08-09 00:20:37 -0400

    Add functionality to update progress bars for lessons in user dashboard #15

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -7,4 +7,18 @@ function setLatestLesson() {
   });
 };
 
+// Important article: https://blog.maximerouiller.com/post/ajax-requests-in-a-loop-and-other-scoping-problems-with-javascript/
+function setLessonProgress() {
+  const demoClass = document.getElementsByClassName('progress-bar');
+  for (var i = 0; i < 3/*demoClass.length*/; i++) {
+    let lessonId = demoClass[i].id;
+    let lesson = lessonId.split("-");
+    $.post("/get_lesson_progress", {username: username, lesson: lesson[1]}, function(data, status) {
+      var percentage = data.progress * 100
+      document.getElementById(lessonId).setAttribute("style", "width: " + percentage + "%;");
+    })
+  };
+};
+
 setLatestLesson();
+setLessonProgress();
