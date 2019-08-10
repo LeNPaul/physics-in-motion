22e3abf568fc29395b58f8973067d3cb5c4b3a1d
Paul Le <l.nguyen.paul@gmail.com>
2019-08-10 17:58:43 -0400

    #2 Remove old Gulp code as they appear to be outdated now

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,4 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
-var nodemon = require('gulp-nodemon');
-
-gulp.task('nodemon', function (done) {
-  nodemon({
-    script: './bin/www'
-  });
-  done();
-});
-
 //  Recreate this configuration
 //  browser-sync start --proxy localhost:8080 --files="public/css/*.css", "public/js/*.js" "public/views/*.pug"  --no-notify
-gulp.task('browser-sync', function (done) {
-  browserSync({
-    proxy: 'localhost:8080',
-    files: ['public/*','views/*'],
-    notify: false
-  });
-  done();
-});
+
+var gulp = require('gulp');
