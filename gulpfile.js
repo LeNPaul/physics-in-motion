82caa6e4deb1382e2e03d826a45b3d61915ff6ee
Paul Le <l.nguyen.paul@gmail.com>
2019-08-10 18:40:10 -0400

    #29 Add Gulp workflow for minifying CSS to a dist directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,20 @@
 //  Recreate this configuration
-//  browser-sync start --proxy localhost:8080 --files="public/css/*.css", "public/js/*.js" "public/views/*.pug"  --no-notify
+//  browser-sync start --proxy localhost:8080 --files="public/css/*.css", "public/js/*.js", "public/views/*.pug"  --no-notify
 
 var gulp = require('gulp');
+var del = require('del');
+var cleanCSS = require('gulp-clean-css');
+
+function clean(cb) {
+  del('dist');
+  cb();
+}
+
+function minifyCss(cb) {
+  gulp.src('public/assets/css/**/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/public/assets/css'));
+  cb();
+}
+
+gulp.task('build', gulp.series(clean, minifyCss))
