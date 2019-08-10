c77f0f9f55dddec7dbead6f812a8f4f6020a0c84
Paul Le <l.nguyen.paul@gmail.com>
2019-08-10 02:29:11 -0400

    Refactor lesson router /update_notes endpoint and simplify code #39

--- routes/lessons.js
+++ routes/lessons.js
@@ -97,101 +97,6 @@ function mapLessons(key, status) {
   return value;
 }
 
-// TODO - move these help functions to their own module
-function mapNotes(key, notes) {
-  var value;
-  switch (key) {
-    // Kinematics
-    case "kinematics.motion_in_one_dimension":
-      value = { $set: { "lesson_modules.kinematics.motion_in_one_dimension.notes": notes } };
-      break;
-    case "kinematics.motion_in_two_dimensions":
-      value = { $set: { "lesson_modules.kinematics.motion_in_two_dimensions.notes": notes } };
-      break;
-    case "kinematics.simple_motion_in_one_dimension":
-      value = { $set: { "lesson_modules.kinematics.simple_motion_in_one_dimension.notes": notes } };
-      break;
-    case "kinematics.simple_motion_in_two_dimensions":
-      value = { $set: { "lesson_modules.kinematics.simple_motion_in_two_dimensions.notes": notes } };
-      break;
-    // Forces
-    case "forces.friction_drag":
-      value = { $set: { "lesson_modules.forces.friction_drag.notes": notes } };
-      break;
-    case "forces.newtons_laws":
-      value = { $set: { "lesson_modules.forces.newtons_laws.notes": notes } };
-      break;
-    case "forces.simple_forces":
-      value = { $set: { "lesson_modules.forces.simple_forces.notes": notes } };
-      break;
-    // Energy
-    case "energy.conservative_forces":
-      value = { $set: { "lesson_modules.energy.conservative_forces.notes": notes } };
-      break;
-    case "energy.energy_conservation_work":
-      value = { $set: { "lesson_modules.energy.energy_conservation_work.notes": notes } };
-      break;
-    case "energy.power":
-      value = { $set: { "lesson_modules.energy.power.notes": notes } };
-      break;
-    case "energy.work_potential_energy":
-      value = { $set: { "lesson_modules.energy.work_potential_energy.notes": notes } };
-      break;
-    // Momentum
-    case "momentum.elastic_collisions":
-      value = { $set: { "lesson_modules.momentum.elastic_collisions.notes": notes } };
-      break;
-    case "momentum.explosions":
-      value = { $set: { "lesson_modules.momentum.explosions.notes": notes } };
-      break;
-    case "momentum.momentum_conservation":
-      value = { $set: { "lesson_modules.momentum.momentum_conservation.notes": notes } };
-      break;
-    // Simple Harmonic Motion
-    case "simple_harmonic_motion.damped_harmonic_motion":
-      value = { $set: { "lesson_modules.simple_harmonic_motion.damped_harmonic_motion.notes": notes } };
-      break;
-    case "simple_harmonic_motion.driven_oscillations":
-      value = { $set: { "lesson_modules.simple_harmonic_motion.driven_oscillations.notes": notes } };
-      break;
-    case "simple_harmonic_motion.dynamics_simple_harmonic_motion":
-      value = { $set: { "lesson_modules.simple_harmonic_motion.dynamics_simple_harmonic_motion.notes": notes } };
-      break;
-    case "simple_harmonic_motion.the_pendulum":
-      value = { $set: { "lesson_modules.simple_harmonic_motion.the_pendulum.notes": notes } };
-      break;
-    // Waves
-    case "waves.characteristics_waves":
-      value = { $set: { "lesson_modules.waves.characteristics_waves.notes": notes } };
-      break;
-    case "waves.interference":
-      value = { $set: { "lesson_modules.waves.interference.notes": notes } };
-      break;
-    case "waves.superposition_of_waves":
-      value = { $set: { "lesson_modules.waves.superposition_of_waves.notes": notes } };
-      break;
-    // Fluids
-    case "fluids.buoyancy":
-      value = { $set: { "lesson_modules.fluids.buoyancy.notes": notes } };
-      break;
-    case "fluids.continuity":
-      value = { $set: { "lesson_modules.fluids.continuity.notes": notes } };
-      break;
-    case "fluids.fluid_statics":
-      value = { $set: { "lesson_modules.fluids.fluid_statics.notes": notes } };
-      break;
-    case "fluids.fluid_dynamics":
-      value = { $set: { "lesson_modules.fluids.fluid_dynamics.notes": notes } };
-      break;
-    case "fluids.incompressible_fluids":
-      value = { $set: { "lesson_modules.fluids.incompressible_fluids.notes": notes } };
-      break;
-    case "fluids.pressure":
-      value = { $set: { "lesson_modules.fluids.pressure.notes": notes } };
-  }
-  return value;
-}
-
 // Updates the updated field to be the current time
 // Sample request: curl --header "Content-Type: application/json" --data '{"username":"paul.le@interfaceware.com", "lessonPath":"forces.friction_drag"}' http://localhost:8080/update_time
 router.post('/update_time', (req, res) => {
@@ -246,9 +151,11 @@ router.post('/update_lesson_status', (req, res) => {
 // Sample request: curl --header "Content-Type: application/json" --data '{"username":"paul.le@interfaceware.com", "lessonPath":"forces.friction_drag", "notes": "Hello world note"}' http://localhost:8080/update_notes
 router.post('/update_notes', (req, res) => {
   Lessons.find({username: req.body.username}, function(err, lessons) {
+    var setObject = {};
+    setObject["lesson_modules." + req.body.lessonPath + ".notes"] = req.body.notes;
     Lessons.findByIdAndUpdate(
       lessons[0]._id,
-      mapNotes(req.body.lessonPath, req.body.notes),
+      setObject,
       { new: true },
       function(err, notes) {
         console.log("Returned the following:");
