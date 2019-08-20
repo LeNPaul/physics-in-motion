45039c4ba059cbc46279fe75a4bc52f84bafc6fd
Paul Le <l.nguyen.paul@gmail.com>
2019-08-20 01:05:36 -0400

    #41 Add some initial code for unit testing the lesson API endpoints

--- test/test.routes.lessons.js
+++ test/test.routes.lessons.js
@@ -1,9 +1,59 @@
 var assert = require('assert');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+const mongoose = require('mongoose');
+let app = require('../app.js');
+const Account = require("../models/account.js");
+const Lessons = require("../models/lessons.js");
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+chai.use(chaiHttp);
+
+describe('/POST user_lessons', () => {
+
+  before((done) => {
+      const db = mongoose.connect('mongodb://localhost/test');
+      done();
+  });
+
+  after((done) => {
+      mongoose.connection.close();
+      done();
+  });
+
+  beforeEach( (done) => {
+      var account = new Account({
+          username: '12345',
+          password: 'testy'
+      });
+      var lesson = new Lessons({
+        username: '12345'
+      });
+      account.save((error) => {
+          if (error) console.log('error' + error.message);
+          else console.log('no error');
+      });
+      lesson.save((error) => {
+          if (error) console.log('error' + error.message);
+          else console.log('no error');
+      });
+      done();
+  });
+
+  it('return user lesson information', (done) => {
+    chai.request(app).post('/user_lessons').send({username:"12345"}).end((err, res) => {
+      res.should.have.status(200);
+      done();
     });
   });
+
+  afterEach((done) => {
+      Account.remove({}, () => {
+
+      });
+      Lessons.remove({}, () => {
+
+      });
+      done();
+   });
+
 });
