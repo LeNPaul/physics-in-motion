400afbacd13873ea4b9dd65134f8b1cf358c298c
Paul Le <l.nguyen.paul@gmail.com>
2019-09-08 15:52:30 -0400

    #40 Implement promises for getting recent lessons and getting lesson progress

--- public/assets/js/dashboardApp.js
+++ public/assets/js/dashboardApp.js
@@ -4,9 +4,33 @@ var dashboardApp = angular.module('dashboardApp', []);
 // create the controller and inject Angular's $scope
 dashboardApp.controller('mainController', function($scope) {
 
-  $.get("/recent_lessons", function(data, status) {
-    $scope.records = data;
-    $scope.$digest();
-  });
+  function getRecentLessons(){
+    return new Promise((resolve, reject) => {
+      $.get("/recent_lessons", function(lessons, status) {
+        resolve(lessons);
+      });
+    })
+  }
+
+  function getLessonProgress(lesson){
+    return new Promise((resolve, reject) => {
+      $.get("/lesson_progress/" + lesson, function(data){
+        resolve(data, lesson);
+      })
+    })
+  }
+
+  getRecentLessons().then(lessons => {
+    for(var i = 0; i < lessons.length; i++) {
+      getLessonProgress(lessons[i][0]).then(data => {
+        // Set the links and stuff
+        console.log(lessons);
+        // Set the lesson progress
+        console.log(data);
+      })
+    }
+  })
+
+  $scope.records = [];
 
 });
