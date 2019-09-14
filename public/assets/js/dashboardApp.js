379c98328b8e9a5dd3e787f8db82916018147de3
Paul Le <l.nguyen.paul@gmail.com>
2019-09-14 14:18:05 -0400

    #40 Generate list of lessons with proper links to lesson module and notes on user dashboard based on most recently viewed lessons

--- public/assets/js/dashboardApp.js
+++ public/assets/js/dashboardApp.js
@@ -4,6 +4,11 @@ var dashboardApp = angular.module('dashboardApp', []);
 // create the controller and inject Angular's $scope
 dashboardApp.controller('mainController', function($scope) {
 
+  // Duplicate of function in noteApp.js
+  function capitalizeFirstLetter(string) {
+    return string.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+  }
+
   function getLessonProgress(lesson){
     return new Promise((resolve, reject) => {
       $.get("/lesson_progress/" + lesson, function(data){
@@ -13,19 +18,22 @@ dashboardApp.controller('mainController', function($scope) {
   }
 
   function getDashboardData(){
-    return new Promise((resolve, reject) => {
-      $.get("/recent_lessons", function(lessons, status) {
-        let dashboardData = [];
-        for (let i=1; i < lessons.length; i++) {
-          getLessonProgress(lessons[i][0]).then(data => {
-            dashboardData.push(lessons[i]);
-          });
-        }
-        resolve(dashboardData);
-        //$scope.records = dashboardData;
-        //$scope.$digest();
-      });
-    })
+    $.get("/recent_lessons", function(lessons, status) {
+      var lessonData = [];
+      console.log(lessons);
+      for (let i = 0; i < lessons.length; i++) {
+        lessonData.push([
+          capitalizeFirstLetter(lessons[i][0].replace(/_/g, ' ')),
+          lessons[i][0].replace(/_/g, '-'),
+          lessons[i][0].replace(/_/g, '-') + "#!/" + lessons[i][1].replace(/_/g, '-'),
+        ]);
+      }
+      console.log(lessonData)
+      $scope.records = lessonData;
+      $scope.$digest();
+    });
   }
+
+  getDashboardData();
 
 });
