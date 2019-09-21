b3e60a309f07e2917604e42125f56198318c2a33
Paul Le <l.nguyen.paul@gmail.com>
2019-09-21 17:27:25 -0500

    #40 Add lesson description for each lesson module on user dashboard

--- public/assets/js/dashboardApp.js
+++ public/assets/js/dashboardApp.js
@@ -9,6 +9,34 @@ dashboardApp.controller('mainController', function($scope) {
     return string.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
   }
 
+  // Temporary solution
+  function getLessonDescription(lessonName) {
+    lessonName = lessonName.toLowerCase();
+    switch(lessonName) {
+      case "kinematics":
+        return "How things move though space. Learn how point objects move though both one-dimensional space, and two-dimensional space."
+        break
+      case "forces":
+        return "What causes things to move through space. Learn about the phenomena that affects the motion of objects through space."
+        break
+      case "energy":
+        return "The 'thing' that does work on an object. It makes things move, and is what makes the universe run at all scales, from the quantum to the astronomical."
+        break
+      case "momentum":
+        return "Momentum is what keeps an avalanche roll once it has been set off - once moving, it takes a large external force to stop!"
+        break
+      case "simple_harmonic_motion":
+        return "Anytime there is something moving back and forth repeatedly in a consisten pattern, you have simple harmonic motion. Think of a pendulum."
+        break
+      case "waves":
+        return "What happens when you have a system of objects interacting with each other through simple harmonic motion? You have waves."
+        break
+      case "fluids":
+        return "Different from point objects - think of so many point objects that they blend into one system known as a fluid. Or, you can just think about water."
+        break
+    }
+  }
+
   // Important article: https://blog.maximerouiller.com/post/ajax-requests-in-a-loop-and-other-scoping-problems-with-javascript/
   function setLessonProgress() {
     const demoClass = document.getElementsByClassName('progress-bar');
@@ -31,7 +59,8 @@ dashboardApp.controller('mainController', function($scope) {
             capitalizeFirstLetter(lessons[i][0].replace(/_/g, ' ')),
             lessons[i][0].replace(/_/g, '-'),
             lessons[i][0].replace(/_/g, '-') + "#!/" + lessons[i][1].replace(/_/g, '-'),
-            lessons[i][0]
+            lessons[i][0],
+            getLessonDescription(lessons[i][0])
           ]);
         }
         $scope.records = lessonData;
