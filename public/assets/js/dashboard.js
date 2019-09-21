b61cbbea23cf1b7edcb454173aa6da714c49e9be
Paul Le <l.nguyen.paul@gmail.com>
2019-09-21 17:03:54 -0500

    #40 Set lesson progress for dynamically generated list of lessons on user dashboard

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -19,4 +19,4 @@ function setLessonProgress() {
 };
 
 setLatestLesson();
-setLessonProgress();
+//setLessonProgress();
--- public/assets/js/dashboardApp.js
+++ public/assets/js/dashboardApp.js
@@ -9,31 +9,40 @@ dashboardApp.controller('mainController', function($scope) {
     return string.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
   }
 
-  function getLessonProgress(lesson){
-    return new Promise((resolve, reject) => {
-      $.get("/lesson_progress/" + lesson, function(data){
-        resolve(data, lesson);
+  // Important article: https://blog.maximerouiller.com/post/ajax-requests-in-a-loop-and-other-scoping-problems-with-javascript/
+  function setLessonProgress() {
+    const demoClass = document.getElementsByClassName('progress-bar');
+    for (var i = 0; i < demoClass.length; i++) {
+      let lessonId = demoClass[i].id;
+      let lesson = lessonId.split("-");
+      $.get("/lesson_progress/" + lesson[1], function(data, status) {
+        var percentage = data.progress * 100
+        document.getElementById(lessonId).setAttribute("style", "width: " + percentage + "%;");
       })
+    };
+  };
+
+  function getDashboardData(){
+    return new Promise((resolve, reject) => {
+      $.get("/recent_lessons", function(lessons, status) {
+        var lessonData = [];
+        for (let i = 0; i < lessons.length; i++) {
+          lessonData.push([
+            capitalizeFirstLetter(lessons[i][0].replace(/_/g, ' ')),
+            lessons[i][0].replace(/_/g, '-'),
+            lessons[i][0].replace(/_/g, '-') + "#!/" + lessons[i][1].replace(/_/g, '-'),
+            lessons[i][0]
+          ]);
+        }
+        $scope.records = lessonData;
+        $scope.$digest();
+        resolve(lessonData);
+      });
     })
   }
 
-  function getDashboardData(){
-    $.get("/recent_lessons", function(lessons, status) {
-      var lessonData = [];
-      console.log(lessons);
-      for (let i = 0; i < lessons.length; i++) {
-        lessonData.push([
-          capitalizeFirstLetter(lessons[i][0].replace(/_/g, ' ')),
-          lessons[i][0].replace(/_/g, '-'),
-          lessons[i][0].replace(/_/g, '-') + "#!/" + lessons[i][1].replace(/_/g, '-'),
-        ]);
-      }
-      console.log(lessonData)
-      $scope.records = lessonData;
-      $scope.$digest();
-    });
-  }
-
-  getDashboardData();
+  getDashboardData().then(function(value){
+    setLessonProgress();
+  })
 
 });
