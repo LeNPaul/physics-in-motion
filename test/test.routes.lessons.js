3c2a41dfb0867f963dbe486cadc91aca9b662d5e
Paul Le <l.nguyen.paul@gmail.com>
2019-09-27 19:37:11 -0500

    #55 Remove test code and add comments for test workflow

--- test/test.routes.lessons.js
+++ test/test.routes.lessons.js
@@ -6,27 +6,13 @@ let app = require('../app.js');
 const Account = require("../models/account.js");
 const Lessons = require("../models/lessons.js");
 
-chai.use(chaiHttp);
-
 describe('Lessons API', () => {
 
   before((done) => {
-    const db = mongoose.connect('mongodb://localhost/test');
-    var account = new Account({
-      username: '12345',
-      password: 'testy'
-    });
-    var lesson = new Lessons({
-      username: '12345'
-    });
-    account.save((error) => {
-      if (error) console.log('error' + error.message);
-      else console.log('no error');
-    });
-    lesson.save((error) => {
-      if (error) console.log('error' + error.message);
-      else console.log('no error');
-    });
+    done();
+  });
+
+  after((done) => {
     done();
   });
 
@@ -34,21 +20,22 @@ describe('Lessons API', () => {
     done();
   });
 
-  it('log into user account', (done) => {
+  // Test workflow:
+  //    1. Create a new Account
+  //    2. Get /lesson_data
+  //    3. Get /recent_lessons
+  //    4. Get /lesson_progress/:lesson for all lessons
+  //    5. Get /notes/:lesson/data for all lessons
+  //    6. Post /update_lesson_time for all lessons
+  //    7 Post /update_lesson_status for all lessons
+  //    8. Post /update_lesson_notes for all lessons
+  //        a. Send a wide range of characters
+
+  it('', (done) => {
     done();
   });
 
   afterEach((done) => {
-    done();
-  });
-
-  after((done) => {
-    Account.remove({}, () => {
-    });
-    Lessons.remove({}, () => {
-    });
-    done();
-    mongoose.connection.close();
     done();
   });
 
