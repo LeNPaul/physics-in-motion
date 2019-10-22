ea9493a7e1dd2a45356607ac7acbaa6fbf9dd12e
Paul Le <l.nguyen.paul@gmail.com>
2019-10-21 22:40:12 -0400

    #68 Add sample code for index route tests

--- test/test.routes.index.js
+++ test/test.routes.index.js
@@ -1,7 +1,15 @@
+const should = require('should');
 var assert = require('assert');
 let chai = require('chai');
+let chaiHttp = require('chai-http');
+let app = require('../app.js');
 
 describe('Route API', () => {
 
+  it('return the home page', (done) => {
+    var test = '12345';
+    test.should.eql('12345');
+    done();
+  });
 
 });
