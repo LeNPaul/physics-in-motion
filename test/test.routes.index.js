915b53d48b277ec9594151b9a61c4bcadedbd4c3
Paul Le <l.nguyen.paul@gmail.com>
2019-10-23 00:28:13 -0500

    #68 Add basic tests for index and about page

--- test/test.routes.index.js
+++ test/test.routes.index.js
@@ -1,15 +1,30 @@
 var should = require('should');
 var assert = require('assert');
-let chai = require('chai');
-let chaiHttp = require('chai-http');
+var chai = require('chai'), chaiHttp = require('chai-http');
 var app = require('../app.js');
 
-describe('Route API', () => {
+chai.use(chaiHttp);
+
+describe('index routes', () => {
 
   it('return the home page', (done) => {
     chai.request(app).get('/').end((err, res) => {
       res.should.have.status(200);
-      done();
+      res.text.length.should.be.eql(5348);
     });
+    done();
+  });
+
+});
+
+describe('about routes', () => {
+
+  it('return the about page', (done) => {
+    chai.request(app).get('/about').end((err, res) => {
+      res.should.have.status(200);
+      res.text.length.should.be.eql(4559);
+    });
+    done();
+  });
 
 });
