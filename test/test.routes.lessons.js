ce01c222624af843922b20dc6a4cf39fc54615ce
Paul Le <l.nguyen.paul@gmail.com>
2019-10-26 01:12:13 -0400

    #55 Move the done() callback function inside the chai.request call

--- test/test.routes.lessons.js
+++ test/test.routes.lessons.js
@@ -5,13 +5,24 @@ var app = 'http://localhost:8080'
 
 chai.use(chaiHttp);
 
+describe('register user', () => {
+
+  it('register a new user', (done) => {
+    chai.request(app).post('/login').send({username: 'test@email.com', password: 'password'}).end((err, res) => {
+      console.log(res);
+      done();
+    });
+  });
+
+});
+
 describe('lesson_data endpoint', () => {
 
   it('return unsuccessful request', (done) => {
     chai.request(app).get('/lesson_data').end((err, res) => {
       res.status.should.be.equal(500);
+      done();
     });
-    done();
   });
 
 });
@@ -21,8 +32,8 @@ describe('recent_lessons endpoint', () => {
   it('return unsuccessful request', (done) => {
     chai.request(app).get('/recent_lessons').end((err, res) => {
       res.status.should.be.equal(500);
+      done();
     });
-    done();
   });
 
 });
@@ -32,8 +43,8 @@ describe('lesson_progress endpoints', () => {
   it('return unsuccessful request', (done) => {
     chai.request(app).get('/lesson_progress/kinematics').end((err, res) => {
       res.status.should.be.equal(500);
+      done();
     });
-    done();
   });
 
 });
