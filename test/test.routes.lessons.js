63835f51d08cc75e8506ba17f699ae5a28888af1
Paul Le <l.nguyen.paul@gmail.com>
2019-10-25 23:46:30 -0400

    #55 Add basic tests for recent_lessons and lesson_progress endpoints

--- test/test.routes.lessons.js
+++ test/test.routes.lessons.js
@@ -15,3 +15,25 @@ describe('lesson_data endpoint', () => {
   });
 
 });
+
+describe('recent_lessons endpoint', () => {
+
+  it('return unsuccessful request', (done) => {
+    chai.request(app).get('/recent_lessons').end((err, res) => {
+      res.status.should.be.equal(500);
+    });
+    done();
+  });
+
+});
+
+describe('lesson_progress endpoints', () => {
+
+  it('return unsuccessful request', (done) => {
+    chai.request(app).get('/lesson_progress/kinematics').end((err, res) => {
+      res.status.should.be.equal(500);
+    });
+    done();
+  });
+
+});
