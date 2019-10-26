540deaad18ad2fc3285a022b2efb1bb425a6c3f2
Paul Le <l.nguyen.paul@gmail.com>
2019-10-26 00:51:26 -0400

    #55 Refactor application server url to be a variable

--- test/test.routes.lessons.js
+++ test/test.routes.lessons.js
@@ -1,13 +1,14 @@
 var should = require('should');
 var assert = require('assert');
 var chai = require('chai'), chaiHttp = require('chai-http');
+var app = 'http://localhost:8080'
 
 chai.use(chaiHttp);
 
 describe('lesson_data endpoint', () => {
 
   it('return unsuccessful request', (done) => {
-    chai.request('http://localhost:8080').get('/lesson_data').end((err, res) => {
+    chai.request(app).get('/lesson_data').end((err, res) => {
       res.status.should.be.equal(500);
     });
     done();
@@ -18,7 +19,7 @@ describe('lesson_data endpoint', () => {
 describe('recent_lessons endpoint', () => {
 
   it('return unsuccessful request', (done) => {
-    chai.request('http://localhost:8080').get('/recent_lessons').end((err, res) => {
+    chai.request(app).get('/recent_lessons').end((err, res) => {
       res.status.should.be.equal(500);
     });
     done();
@@ -29,7 +30,7 @@ describe('recent_lessons endpoint', () => {
 describe('lesson_progress endpoints', () => {
 
   it('return unsuccessful request', (done) => {
-    chai.request('http://localhost:8080').get('/lesson_progress/kinematics').end((err, res) => {
+    chai.request(app).get('/lesson_progress/kinematics').end((err, res) => {
       res.status.should.be.equal(500);
     });
     done();
