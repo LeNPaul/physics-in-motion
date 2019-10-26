32aa42de930e000772e91c4a9b93869021729d2a
Paul Le <l.nguyen.paul@gmail.com>
2019-10-26 00:50:36 -0400

    #68 Refactor application server url to be a variable

--- test/test.routes.index.js
+++ test/test.routes.index.js
@@ -1,13 +1,14 @@
 var should = require('should');
 var assert = require('assert');
 var chai = require('chai'), chaiHttp = require('chai-http');
+var app = 'http://localhost:8080'
 
 chai.use(chaiHttp);
 
 describe('index routes', () => {
 
   it('return the home page', (done) => {
-    chai.request('http://localhost:8080').get('/').end((err, res) => {
+    chai.request(app).get('/').end((err, res) => {
       res.status.should.be.equal(200);
       res.text.length.should.be.eql(5348);
     });
@@ -19,7 +20,7 @@ describe('index routes', () => {
 describe('about routes', () => {
 
   it('return the about page', (done) => {
-    chai.request('http://localhost:8080').get('/about').end((err, res) => {
+    chai.request(app).get('/about').end((err, res) => {
       res.status.should.be.equal(200);
       res.text.length.should.be.eql(4559);
     });
@@ -31,7 +32,7 @@ describe('about routes', () => {
 describe('contact routes', () => {
 
   it('return the contact page', (done) => {
-    chai.request('http://localhost:8080').get('/contact').end((err, res) => {
+    chai.request(app).get('/contact').end((err, res) => {
       res.status.should.be.equal(200);
       res.text.length.should.be.eql(3553);
     });
@@ -43,7 +44,7 @@ describe('contact routes', () => {
 describe('privacy routes', () => {
 
   it('return the privacy page', (done) => {
-    chai.request('http://localhost:8080').get('/privacy').end((err, res) => {
+    chai.request(app).get('/privacy').end((err, res) => {
       res.status.should.be.equal(200);
       res.text.length.should.be.eql(11086);
     });
@@ -55,7 +56,7 @@ describe('privacy routes', () => {
 describe('terms routes', () => {
 
   it('return the terms page', (done) => {
-    chai.request('http://localhost:8080').get('/terms').end((err, res) => {
+    chai.request(app).get('/terms').end((err, res) => {
       res.status.should.be.equal(200);
       res.text.length.should.be.eql(7858);
     });
