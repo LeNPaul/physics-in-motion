5a975ba1c8ba24554e8d3a202e76500f6fce43e8
Paul Le <l.nguyen.paul@gmail.com>
2019-11-18 20:47:56 -0800

    #68 Update tests for index.js routes

--- test/test.routes.index.js
+++ test/test.routes.index.js
@@ -122,170 +122,323 @@ describe('routes/index.js endpoints', () => {
   });
 
   describe('/contact endpoint', () => {
-    it('return the contact page', (done) => {
+    it('return the contact page with user not logged in', (done) => {
       chai.request(app).get('/contact').end((err, res) => {
         res.status.should.be.equal(200);
         res.text.length.should.be.eql(3553);
       });
       done();
     });
+    it('return the contact page with user logged in', (done) => {
+      agent.get('/contact').end((err, res) => {
+        res.status.should.be.equal(200);
+        res.text.length.should.be.eql(3807);
+        res.text.includes('Logged in as').should.be.equal(true);
+        res.text.includes('Logout').should.be.equal(true);
+      });
+      done();
+    });
   });
 
   describe('/privacy endpoint', () => {
-    it('return the privacy page', (done) => {
+    it('return the privacy page with user not logged in', (done) => {
       chai.request(app).get('/privacy').end((err, res) => {
         res.status.should.be.equal(200);
         res.text.length.should.be.eql(11086);
       });
       done();
     });
+    it('return the privacy page with user logged in', (done) => {
+      agent.get('/privacy').end((err, res) => {
+        res.status.should.be.equal(200);
+        res.text.length.should.be.eql(11340);
+        res.text.includes('Logged in as').should.be.equal(true);
+        res.text.includes('Logout').should.be.equal(true);
+      });
+      done();
+    });
   });
 
   describe('/terms endpoint', () => {
-    it('return the terms page', (done) => {
+    it('return the terms page with user not logged in', (done) => {
       chai.request(app).get('/terms').end((err, res) => {
         res.status.should.be.equal(200);
         res.text.length.should.be.eql(7858);
       });
       done();
     });
+    it('return the terms page with user logged in', (done) => {
+      agent.get('/terms').end((err, res) => {
+        res.status.should.be.equal(200);
+        res.text.length.should.be.eql(8112);
+        res.text.includes('Logged in as').should.be.equal(true);
+        res.text.includes('Logout').should.be.equal(true);
+      });
+      done();
+    });
   });
 
   describe('/courses', () => {
-    it('return the courses page', (done) => {
+    it('return the courses page with user not logged in', (done) => {
       chai.request(app).get('/courses').end((err, res) => {
         res.status.should.be.equal(200);
         res.text.length.should.be.eql(5056);
       });
       done();
     });
+    it('return the courses page with user logged in', (done) => {
+      agent.get('/courses').end((err, res) => {
+        res.status.should.be.equal(200);
+        res.text.length.should.be.eql(5310);
+        res.text.includes('Logged in as').should.be.equal(true);
+        res.text.includes('Logout').should.be.equal(true);
+      });
+      done();
+    });
   });
 
   describe('/classical-mechanics', () => {
-    it('return the classical-mechanics page', (done) => {
+    it('return the classical-mechanics page with user not logged in', (done) => {
       chai.request(app).get('/classical-mechanics').end((err, res) => {
         res.status.should.be.equal(200);
         res.text.length.should.be.eql(7309);
       });
       done();
     });
+    it('return the classical-mechanics page with user logged in', (done) => {
+      agent.get('/classical-mechanics').end((err, res) => {
+        res.status.should.be.equal(200);
+        res.text.length.should.be.eql(7563);
+        res.text.includes('Logged in as').should.be.equal(true);
+        res.text.includes('Logout').should.be.equal(true);
+      });
+      done();
+    });
   });
 
   describe('/kinematics', () => {
-    it('return the kinematics page', (done) => {
+    it('return the kinematics page with user not logged in', (done) => {
       chai.request(app).get('/kinematics').end((err, res) => {
         res.status.should.be.equal(200);
         res.text.length.should.be.eql(4482);
       });
       done();
     });
+    it('return the kinematics page with user logged in', (done) => {
+      agent.get('/kinematics').end((err, res) => {
+        res.status.should.be.equal(200);
+        res.text.length.should.be.eql(5495);
+        res.text.includes('Logged in as').should.be.equal(true);
+        res.text.includes('Logout').should.be.equal(true);
+      });
+      done();
+    });
   });
 
   describe('/forces', () => {
-    it('return the forces page', (done) => {
+    it('return the forces page with user not logged in', (done) => {
       chai.request(app).get('/forces').end((err, res) => {
         res.status.should.be.equal(200);
         res.text.length.should.be.eql(4279);
       });
       done();
     });
+    it('return the forces page with user logged in', (done) => {
+      agent.get('/forces').end((err, res) => {
+        res.status.should.be.equal(200);
+        res.text.length.should.be.eql(5292);
+        res.text.includes('Logged in as').should.be.equal(true);
+        res.text.includes('Logout').should.be.equal(true);
+      });
+      done();
+    });
   });
 
   describe('/energy', () => {
-    it('return the energy page', (done) => {
+    it('return the energy page with user not logged in', (done) => {
       chai.request(app).get('/energy').end((err, res) => {
         res.status.should.be.equal(200);
         res.text.length.should.be.eql(4404);
       });
       done();
     });
+    it('return the energy page with user logged in', (done) => {
+      agent.get('/energy').end((err, res) => {
+        res.status.should.be.equal(200);
+        res.text.length.should.be.eql(5417);
+        res.text.includes('Logged in as').should.be.equal(true);
+        res.text.includes('Logout').should.be.equal(true);
+      });
+      done();
+    });
   });
 
   describe('/momentum', () => {
-    it('return the momentum page', (done) => {
+    it('return the momentum page with user not logged in', (done) => {
       chai.request(app).get('/momentum').end((err, res) => {
         res.status.should.be.equal(200);
         res.text.length.should.be.eql(4302);
       });
       done();
     });
+    it('return the momentum page with user logged in', (done) => {
+      agent.get('/momentum').end((err, res) => {
+        res.status.should.be.equal(200);
+        res.text.length.should.be.eql(5315);
+        res.text.includes('Logged in as').should.be.equal(true);
+        res.text.includes('Logout').should.be.equal(true);
+      });
+      done();
+    });
   });
 
   describe('/simple-harmonic-motion', () => {
-    it('return the simple-harmonic-motion page', (done) => {
+    it('return the simple-harmonic-motion page with user not logged in', (done) => {
       chai.request(app).get('/simple-harmonic-motion').end((err, res) => {
         res.status.should.be.equal(200);
         res.text.length.should.be.eql(4436);
       });
       done();
     });
+    it('return the simple-harmonic-motion page with user logged in', (done) => {
+      agent.get('/simple-harmonic-motion').end((err, res) => {
+        res.status.should.be.equal(200);
+        res.text.length.should.be.eql(5449);
+        res.text.includes('Logged in as').should.be.equal(true);
+        res.text.includes('Logout').should.be.equal(true);
+      });
+      done();
+    });
   });
 
   describe('/waves', () => {
-    it('return the waves page', (done) => {
+    it('return the waves page with user not logged in', (done) => {
       chai.request(app).get('/waves').end((err, res) => {
         res.status.should.be.equal(200);
         res.text.length.should.be.eql(4311);
       });
       done();
     });
+    it('return the waves page with user logged in', (done) => {
+      agent.get('/waves').end((err, res) => {
+        res.status.should.be.equal(200);
+        res.text.length.should.be.eql(5324);
+        res.text.includes('Logged in as').should.be.equal(true);
+        res.text.includes('Logout').should.be.equal(true);
+      });
+      done();
+    });
   });
 
   describe('/fluids', () => {
-    it('return the fluids page', (done) => {
+    it('return the fluids page with user not logged in', (done) => {
       chai.request(app).get('/fluids').end((err, res) => {
         res.status.should.be.equal(200);
         res.text.length.should.be.eql(4425);
       });
       done();
     });
+    it('return the fluids page with user logged in', (done) => {
+      agent.get('/fluids').end((err, res) => {
+        res.status.should.be.equal(200);
+        res.text.length.should.be.eql(5438);
+        res.text.includes('Logged in as').should.be.equal(true);
+        res.text.includes('Logout').should.be.equal(true);
+      });
+      done();
+    });
   });
 
   describe('/mathematics', () => {
-    it('return the mathematics page', (done) => {
+    it('return the mathematics page with user not logged in', (done) => {
       chai.request(app).get('/mathematics').end((err, res) => {
         res.status.should.be.equal(200);
         res.text.length.should.be.eql(5106);
       });
       done();
     });
+    it('return the mathematics page with user logged in', (done) => {
+      agent.get('/mathematics').end((err, res) => {
+        res.status.should.be.equal(200);
+        res.text.length.should.be.eql(5360);
+        res.text.includes('Logged in as').should.be.equal(true);
+        res.text.includes('Logout').should.be.equal(true);
+      });
+      done();
+    });
   });
 
   describe('/algebra', () => {
-    it('return the algebra page', (done) => {
+    it('return the algebra page with user not logged in', (done) => {
       chai.request(app).get('/algebra').end((err, res) => {
         res.status.should.be.equal(200);
         res.text.length.should.be.eql(4358);
       });
       done();
     });
+    it('return the algebra page with user logged in', (done) => {
+      agent.get('/algebra').end((err, res) => {
+        res.status.should.be.equal(200);
+        res.text.length.should.be.eql(5371);
+        res.text.includes('Logged in as').should.be.equal(true);
+        res.text.includes('Logout').should.be.equal(true);
+      });
+      done();
+    });
   });
 
   describe('/geometry', () => {
-    it('return the geometry page', (done) => {
+    it('return the geometry page with user not logged in', (done) => {
       chai.request(app).get('/geometry').end((err, res) => {
         res.status.should.be.equal(200);
         res.text.length.should.be.eql(4554);
       });
       done();
     });
+    it('return the geometry page with user logged in', (done) => {
+      agent.get('/geometry').end((err, res) => {
+        res.status.should.be.equal(200);
+        res.text.length.should.be.eql(5567);
+        res.text.includes('Logged in as').should.be.equal(true);
+        res.text.includes('Logout').should.be.equal(true);
+      });
+      done();
+    });
   });
 
   describe('/calculus', () => {
-    it('return the calculus page', (done) => {
+    it('return the calculus page with user not logged in', (done) => {
       chai.request(app).get('/calculus').end((err, res) => {
         res.status.should.be.equal(200);
         res.text.length.should.be.eql(4244);
       });
       done();
     });
+    it('return the calculus page with user logged in', (done) => {
+      agent.get('/calculus').end((err, res) => {
+        res.status.should.be.equal(200);
+        res.text.length.should.be.eql(5257);
+        res.text.includes('Logged in as').should.be.equal(true);
+        res.text.includes('Logout').should.be.equal(true);
+      });
+      done();
+    });
   });
 
   describe('/resources', () => {
-    it('return the resources page', (done) => {
+    it('return the resources page with user not logged in', (done) => {
       chai.request(app).get('/resources').end((err, res) => {
         res.status.should.be.equal(200);
         res.text.length.should.be.eql(8500);
+      });
+      done();
+    });
+    it('return the resources page with user logged in', (done) => {
+      agent.get('/resources').end((err, res) => {
+        res.status.should.be.equal(200);
+        res.text.length.should.be.eql(8754);
+        res.text.includes('Logged in as').should.be.equal(true);
+        res.text.includes('Logout').should.be.equal(true);
       });
       done();
     });
@@ -302,6 +455,7 @@ describe('routes/index.js endpoints', () => {
         });
         it('get user lesson notes for /notes/' + lessons[i][0], (done) => {
           agent.get('/notes/' + lessons[i][0]).end((err, res) => {
+            console.log(res);
             res.status.should.be.equal(200);
             done();
           });
