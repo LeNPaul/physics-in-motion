91406f3debfe8e2de0b41bdc91c9b5ccd9b40df4
Paul Le <l.nguyen.paul@gmail.com>
2019-11-23 15:57:42 -0800

    #67 Add stub code for API endpoint for saving quiz response for a user

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const Quizzes = require('../models/quizzes');
 
-router.get('/quiz', (req, res) => {
-  var newQuiz = new Quizzes({
-    username: "test user"
-  })
-  newQuiz.save(function(err, data) {
-    if (err) {
-      // TODO - add proper error handling here
-    }
-  })
+// Accepts a question_id and returns 5 answers - 1 correct and 4 incorrect
+router.get('/answers', (req, res) => {
+  res.json({});
+});
+
+// Submit a quiz response for a lesson module
+// Update the database to include user's last response and time
+// Response will include question_id and answer_id - the response will indicate if correct or not
+router.post('/submit_response', (req, res) => {
   res.json({Success: true});
 });
 
