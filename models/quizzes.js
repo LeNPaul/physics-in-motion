19249b740c69161d3610a1029ded0a09eef065ba
Paul Le <l.nguyen.paul@gmail.com>
2019-11-23 15:21:19 -0800

    #65 Update Quizzes database schema to include module name and initiate certain fields with an empty string as the default value

--- models/quizzes.js
+++ models/quizzes.js
@@ -3,10 +3,11 @@ const Schema = mongoose.Schema;
 
 const Quizzes = new Schema({
   username: String,
+  module_name: String,
   lesson_name: String,
   question_id: String,
-  last_response_answer_id: String,
-  last_response_time: String
+  last_response_answer_id: { type: String, default: "" },
+  last_response_time: { type: String, default: "" }
 });
 
 module.exports = mongoose.model('quizzes', Quizzes);
