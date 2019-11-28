8ffaf5051432ec7570af9cf0a5c403c38a7a704a
Paul Le <l.nguyen.paul@gmail.com>
2019-11-27 18:31:23 -0800

    #67 Add code to return quiz questions for a lesson module for a user from the /questions/:lesson_name endpoint

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -2,19 +2,23 @@ const express = require('express');
 const router = express.Router();
 const Quizzes = require('../models/quizzes');
 
-// Accepts a username and lesson_module name
+// Accepts lesson_name name
 // Returns the questions for that lesson module
 //  Returned as an array with 5 elements
 //    Each element must be an object with question text
-router.get('/questions', (req, res) => {
-  res.json([]);
+// Sample request: curl --cookie "" --data '{}' http://localhost:8080/questions
+// TODO: filter for 5 random rows
+router.get('/questions/:lesson_name', (req, res) => {
+  Quizzes.find({username: req.user.username, lesson_name: req.params.lesson_name}, function(err, lessons) {
+    res.json(lessons);
+  });
 });
 
 // Accepts a question_id
 // Returns 5 answers - 1 correct and 4 incorrect
 //  Returned as an array with 5 elements
 //    Each element much be an object with answer text
-router.get('/answers', (req, res) => {
+router.get('/answers/:question_id', (req, res) => {
   res.json([]);
 });
 
