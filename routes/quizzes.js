2ef599e9260b6b144f4ba6e894b477af04c1af3c
Paul Le <l.nguyen.paul@gmail.com>
2019-12-14 00:30:14 -0500

    #67 Update comments; remove limitation to return only one correct answer for /answers/:question_id endpoint

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -3,12 +3,10 @@ const router = express.Router();
 const Quizzes = require('../models/quizzes');
 const Answers = require('../models/answers');
 
-// Accepts lesson_name name
+// Accepts lesson_name
 // Returns the questions for that lesson module
-//  Returned as an array with 5 elements
-//    Each element must be an object with question text
+// Each element must be an object with question text
 // Sample request: curl --cookie "" --data '{}' http://localhost:8080/questions
-// TODO: filter for 5 random rows
 router.get('/questions/:lesson_name', (req, res) => {
   Quizzes.find({username: req.user.username, lesson_name: req.params.lesson_name}, function(err, questions) {
     res.json(questions);
@@ -16,19 +14,17 @@ router.get('/questions/:lesson_name', (req, res) => {
 });
 
 // Accepts a question_id
-// Returns 5 answers - 1 correct and 4 incorrect
-//  Returned as an array with 5 elements
-//    Each element much be an object with answer text
-// TODO: somone fetch 1 correct and 4 incorrect
+// Returns answers with at least 1 correct answer
+// Each element must be an object with answer text
 router.get('/answers/:question_id', (req, res) => {
-
   var username = req.user.username; // This is workaround to prevent application from crashing
-
-  // Get one answer that is true
-  Answers.find({question_id: req.params.question_id, is_correct: true}, function(err, correct_answer) {
+  // Get correct answers
+  Answers.find({question_id: req.params.question_id, is_correct: true}, function(err, correct_answers) {
     var answerResponse = [];
-    answerResponse.push(correct_answer[0]);
-    // Get four answers that are false
+    for (let i=0; i < correct_answers.length; i++) {
+      answerResponse.push(correct_answers[i]);
+    };
+    // Get incorrect answers
     Answers.find({question_id: req.params.question_id, is_correct: false}, function(err, incorrect_answers) {
       for (let i=0; i < incorrect_answers.length; i++) {
         answerResponse.push(incorrect_answers[i]);
@@ -41,15 +37,11 @@ router.get('/answers/:question_id', (req, res) => {
 // Submit a quiz response for a lesson module
 // Update the database to include user's last response and time
 // Accepts question_id and answer_id
-// Returns the response will indicate if correct or not - i.e. {Success: true}
-//   Returned as an object with a field of Success that is equal to true or false
-
+// Returns the response to indicate if correct or not - i.e. {is_correct: true}
+// Returned as an object with a field of is_correct that is equal to true or false
 // curl --header "Content-Type: application/json" --data '{"question_id": "1", "answer_id": "3456"}' http://localhost:8080/submit_response
-
 router.post('/submit_response', (req, res) => {
-
   var username = req.user.username; // This is workaround to prevent application from crashing
-
   // Find user's quiz and update last edit time
   Quizzes.find({username: 'test@email.com', question_id: req.body.question_id}, function(err, quizzes) {
     // Update last edit time
