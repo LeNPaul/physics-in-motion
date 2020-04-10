1b8069afd0da3f5f98bed1277d80a11817b5033f
Paul Le <l.nguyen.paul@gmail.com>
2020-04-09 23:53:39 -0400

    #121 Display top 10 users on the administrator dashboard

--- public/assets/js/adminApp.js
+++ public/assets/js/adminApp.js
@@ -4,6 +4,9 @@ var noteApp = angular.module('adminApp', []);
 // create the controller and inject Angular's $scope
 noteApp.controller('mainController', function($scope) {
 
-  $scope.test = 'Hello World!'
+  $.get('/user_list?page=0&limit=10', function(data, status) {
+    $scope.Users = data;
+    $scope.$digest();
+  });
 
 });
