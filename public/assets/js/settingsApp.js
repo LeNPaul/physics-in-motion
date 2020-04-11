9f6c1655dec103d511332629a7f9cb5ed5c4f61a
Paul Le <l.nguyen.paul@gmail.com>
2020-04-11 00:10:40 -0400

    #86 Confirm that user wants to delete account before deleting account

--- public/assets/js/settingsApp.js
+++ public/assets/js/settingsApp.js
@@ -4,7 +4,9 @@ var noteApp = angular.module('settingsApp', []);
 // create the controller and inject Angular's $scope
 noteApp.controller('mainController', function($scope, $window) {
 
-    $scope.deleteUser = function() {
+  $scope.deleteUser = function() {
+    let isDelete = confirm("Are you sure you would like to delete your account? This is not reversable.");
+    if ( isDelete ) {
       var username = $('#username').text();
       $.post("/delete_user", {user: username}, function(data, status) {
         // Delete user and reload the page
@@ -12,5 +14,6 @@ noteApp.controller('mainController', function($scope, $window) {
         $window.location.href = '/';
       });
     }
-
+  }
+  
 });
