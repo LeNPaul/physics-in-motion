f87e455b09cb759d152a9c31de7941ea7229a244
Paul Le <l.nguyen.paul@gmail.com>
2020-04-16 21:04:53 -0400

    #30 Move quadratic and linear functions to math module and delete identical functions

--- public/assets/js/math.js
+++ public/assets/js/math.js
@@ -2,3 +2,11 @@ function generateRandomNumber(min_value, max_value) {
   let random_number = Math.random() * (max_value - min_value) + min_value;
   return Math.floor(random_number);
 }
+
+function quadraticFunction(time, a, c) {
+  return a * (time * time) + c
+}
+
+function linearFunction(time, a, c) {
+  return a * time + c
+}
