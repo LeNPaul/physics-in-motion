45539d13f594c333d4e9a8723a1eb544526fc08f
Paul Le <l.nguyen.paul@gmail.com>
2020-04-25 23:46:57 -0400

    #131 Dynamically create link to posts on News & Updates page

--- public/assets/js/postApp.js
+++ public/assets/js/postApp.js
@@ -4,12 +4,22 @@ var postApp = angular.module('postApp', ['ngRoute']);
 // configure our routes
 postApp.config(function($routeProvider) {
   $routeProvider
+
+  .when('/news-and-updates', {
+    templateUrl: 'posts/news-and-updates.html',
+  })
+
   .when('/2017-09-11-welcome-to-beta-release', {
     templateUrl: 'posts/2017-09-11-welcome-to-beta-release.html',
   })
+
 });
 
 // create the controller and inject Angular's $scope
 postApp.controller('mainController', function($scope) {
+
+  $scope.posts = [
+    {'title': 'Welcome to the Beta Release of Physics in Motion!', 'description': 'This is only a beta release, with only a few courses created.', 'link': '2017-09-11-welcome-to-beta-release'}
+  ];
 
 });
