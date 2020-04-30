95490703123df7006cfc141792796a1ac0ccae19
Paul Le <l.nguyen.paul@gmail.com>
2020-04-29 22:07:16 -0400

    #134 Update user profile when navigating to the user settings page

--- public/assets/js/settingsApp.js
+++ public/assets/js/settingsApp.js
@@ -4,6 +4,28 @@ var noteApp = angular.module('settingsApp', []);
 // create the controller and inject Angular's $scope
 noteApp.controller('mainController', function($scope, $window) {
 
+  // Populate user information as placeholder text
+  // When pressing update, get form data that has been filled and make request to backend
+
+  $scope.getUserInfo = function() {
+    $.get("/userinfo", function(data, status) {
+      $scope.username = data.username;
+      $scope.email = data.email;
+      $scope.name = data.name;
+      $scope.gender = data.gender;
+      $scope.birthdate = data.birthdate;
+      $scope.address = data.address;
+      $scope.$digest();
+    });
+  }
+
+  // Get user information and update form
+  $scope.getUserInfo();
+
+  $scope.updateUserInfo = function() {
+    
+  }
+
   $scope.deleteUser = function() {
     let isDelete = confirm("Are you sure you would like to delete your account? This is not reversable.");
     if ( isDelete ) {
@@ -15,5 +37,5 @@ noteApp.controller('mainController', function($scope, $window) {
       });
     }
   }
-  
+
 });
