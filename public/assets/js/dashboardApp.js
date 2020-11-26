0438f9cf9d633380f3ae3053fea8b0dbb4483d1a
Paul Le <l.nguyen.paul@gmail.com>
2020-11-25 21:51:13 -0500

    #149 Add routing for blog post announcing official release

--- public/assets/js/dashboardApp.js
+++ public/assets/js/dashboardApp.js
@@ -25,6 +25,7 @@ dashboardApp.controller('mainController', function($scope) {
 
   // Duplicate of postApp.js
   $scope.posts = [
+    {'title': 'Welcome to the First Release of Physics in Motion!', 'description': 'This is now the official first release of Physics in Motion! Stay tuned for more to come!', 'link': '2020-11-09-welcome-to-first-release'},
     {'title': 'Welcome to the Beta Release of Physics in Motion!', 'description': 'This is only a beta release, with only a few courses created.', 'link': '2020-04-25-welcome-to-beta-release'},
     {'title': 'Updates to our Terms and Privacy', 'description': 'We have recently updated our Terms and Privacy pages.', 'link': '2020-01-05-updates-to-terms-and-privacy'}
   ];
--- public/assets/js/postApp.js
+++ public/assets/js/postApp.js
@@ -17,12 +17,17 @@ postApp.config(function($routeProvider) {
     templateUrl: 'posts/2020-04-25-welcome-to-beta-release.html',
   })
 
+  .when('/2020-11-01-welcome-to-first-release', {
+    templateUrl: 'posts/2020-11-09-welcome-to-first-release.html',
+  })
+
 });
 
 // create the controller and inject Angular's $scope
 postApp.controller('mainController', function($scope) {
 
   $scope.posts = [
+    {'title': 'Welcome to the First Release of Physics in Motion!', 'description': 'This is now the official first release of Physics in Motion! Stay tuned for more to come!', 'link': '2020-11-01-welcome-to-first-release'},
     {'title': 'Welcome to the Beta Release of Physics in Motion!', 'description': 'This is only a beta release, with only a few courses created.', 'link': '2020-04-25-welcome-to-beta-release'},
     {'title': 'Updates to our Terms and Privacy', 'description': 'We have recently updated our Terms and Privacy pages.', 'link': '2020-01-05-updates-to-terms-and-privacy'}
   ];
