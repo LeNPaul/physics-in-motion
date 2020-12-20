c65d64f1e4a693492aed8cd36fd3e46643ca73a9
Paul Le <l.nguyen.paul@gmail.com>
2020-12-20 17:08:52 -0500

    #62 Add script for installing nginx and updating firewall

--- server/install/install.sh
+++ server/install/install.sh
@@ -12,3 +12,7 @@ curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt install -y nodejs
 sudo apt install -y npm
+
+# Install nginx and update firewall
+sudo apt install -y nginx
+sudo ufw allow 'Nginx HTTP'
