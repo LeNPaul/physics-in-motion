57a3e3e0ce530330a86e4cdd7c9fc4b98481f193
Paul Le <l.nguyen.paul@gmail.com>
2020-12-20 16:01:36 -0500

    #62 Add script for setting up NodeJS and npm

--- server/install/install.sh
+++ server/install/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Update and install git
 sudo apt update
+sudo apt install -y git
 
 # Install MongoDB
 sudo apt install -y mongodb
+
+# Install NodeJS
+sudo apt install nodejs
+sudo apt install npm
