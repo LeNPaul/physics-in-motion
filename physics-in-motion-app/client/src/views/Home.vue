7baceea38e64ad6e610c3fb8b3f52ed062b98767
Paul Le <l.nguyen.paul@gmail.com>
2021-11-16 23:21:00 -0500

    #161 Add p5.js example of spring

--- physics-in-motion-app/client/src/views/Home.vue
+++ physics-in-motion-app/client/src/views/Home.vue
@@ -44,30 +44,86 @@ export default {
   name: 'Home',
   mounted() {
     const script = function (p5) {
-      var speed = 2;
-      var posX = 0;
-      // NOTE: Set up is here
+      // Define variables here
+      // Spring drawing constants for top bar
+      let springHeight = 32,
+          left,
+          right,
+          maxHeight = 200,
+          minHeight = 100,
+          over = false,
+          move = false;
+      // Spring simulation constants
+      let M = 0.8,  // Mass
+          K = 0.2,  // Spring constant
+          D = 0.92, // Damping
+          R = 150;  // Rest position
+      // Spring simulation variables
+      let ps = R,   // Position
+          vs = 0.0, // Velocity
+          as = 0,   // Acceleration
+          f = 0;    // Force
+      // Set up is here
       p5.setup = _ => {
-        p5.createCanvas(500, 500).parent("p5Canvas");
-        p5.ellipse(p5.width / 2, p5.height / 2, 500, 500);
+        p5.createCanvas(400, 400).parent("p5Canvas");
+        p5.rectMode(p5.CORNERS);
+        p5.noStroke();
+        left = p5.width / 2 - 100;
+        right = p5.width / 2 + 100;
       }
       // NOTE: Draw is here
       p5.draw = _ => {
-        p5.background(0);
-        const degree = p5.frameCount * 3;
-        const y = p5.sin(p5.radians(degree)) * 50;
-        p5.push();
-        p5.translate(0, p5.height / 2);
-        p5.ellipse(posX, y, 50, 50);
-        p5.pop();
-        posX += speed;
-
-        if (posX > p5.width || posX < 0) {
-          speed *= -1;
+        // Black background for now
+        p5.background(102);
+        updateSpring();
+        drawSpring();
+      }
+      function drawSpring() {
+        // Draw base
+        p5.fill(0.2);
+        let baseWidth = 0.5 * ps + -8;
+        p5.rect(p5.width / 2 - baseWidth, ps + springHeight, p5.width / 2 + baseWidth, p5.height);
+        // Set color and draw top bar
+        if (over || move) {
+          p5.fill(255);
+        } else {
+          p5.fill(204);
+        }
+        p5.rect(left, ps, right, ps + springHeight);
+      }
+      function updateSpring() {
+        // Update the spring position
+        if ( !move ) {
+          f = -K * ( ps - R ); // f=-ky
+          as = f / M;          // Set the acceleration, f=ma == a=f/m
+          vs = D * (vs + as);  // Set the velocity
+          ps = ps + vs;        // Updated position
+        }
+        if (p5.abs(vs) < 0.1) {
+          vs = 0.0;
+        }
+        // Test if mouse if over the top bar
+        if (p5.mouseX > left && p5.mouseX < right && p5.mouseY > ps && p5.mouseY < ps + springHeight) {
+          over = true;
+        } else {
+          over = false;
+        }
+        // Set and constrain the position of top bar
+        if (move) {
+          ps = p5.mouseY - springHeight / 2;
+          ps = p5.constrain(ps, minHeight, maxHeight);
         }
       }
+       p5.mousePressed = _ => {
+        if (over) {
+          move = true;
+        }
+      }
+      p5.mouseReleased = _ => {
+        move = false;
+      }
     }
-    // NOTE: Use p5 as an instance mode
+    // Use p5 as an instance mode
     const P5 = require('p5');
     new P5(script)
   }
