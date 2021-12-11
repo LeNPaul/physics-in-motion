088ee0bed8583e548b77b3489b02ffdde11aed85
Paul Le <l.nguyen.paul@gmail.com>
2021-12-11 14:57:23 -0500

    #161 Update About page

--- physics-in-motion-app/client/src/views/About.vue
+++ physics-in-motion-app/client/src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+
+  <h1>About Us</h1>
+
+  <p class="col-md-8">Education is not something that you can ever finish, and should never stop once you complete school. Unfortunately, traditional school values have raised people to view learning as a chore, something that you only do a means to end. Many can’t wait until they finish school so that they can stop studying for school and start working at a job.</p>
+
+  <p class="col-md-8">My goal is to change this, and to demonstrate that learning is something that should be done for its own sake. In particular, the study of physics often gets a bad reputation for being a difficult and even boring subject to study (it isn’t!). A big part of why this is the case is due to how physics is taught. Physics, by its very nature, is the study of motion, and yet when it is taught in school, we typically see static drawings instead of dynamic demonstrations.</p>
+
+  <p class="col-md-8">Physics in Motion is a free online resource for learning physics, whether you are a student learning physics in school, or an enthusiast looking to learn something new, this site offers a comprehensive resource for learning physics how it was meant to be taught.</p>
+  
 </template>
