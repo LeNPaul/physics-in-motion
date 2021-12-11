4f6d8004cfdd9ee963a7504001069477b0dc203b
Paul Le <l.nguyen.paul@gmail.com>
2021-12-11 14:32:07 -0500

    #161 Add route for Hooke's Law and move content from the home page to this route

--- physics-in-motion-app/client/src/router/index.js
+++ physics-in-motion-app/client/src/router/index.js
@@ -14,6 +14,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/hookes-law',
+    name: `Hooke's Law`,
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/HookesLaw.vue')
   }
 ]
 
