a4c0bcb439f6ac242ddf68b09d48eae545e9cf56
Paul Le <l.nguyen.paul@gmail.com>
2021-12-12 23:12:42 -0500

    #162 Update home page, motion in one dimension page, and position in one dimension page

--- physics-in-motion-app/client/src/views/Home.vue
+++ physics-in-motion-app/client/src/views/Home.vue
@@ -1,4 +1,5 @@
 <template>
+
   <h1>How physics should be taught.</h1>
   <p class="fs-5 col-md-8">Not through static diagrams and long blocks of meaningless text, but through dynamic physical simulations that allow you to build an intuition for how physical systems actually work.</p>
   <div class="mb-5">
@@ -11,12 +12,5 @@
       <p>Start with the most basic example of physics in motion: motion in one dimension. Learn about position, velocity, and acceleration. <router-link to="/one-dimensional-motion" class="text-dark"><i class="fas fa-arrow-right"></i></router-link></p>
     </div>
   </div>
-
-  <div class="row g-5 mb-5">
-    <div class="col-md-6">
-      <h2>Hooke's Law</h2>
-      <p>Springs exist all around us, in the form of things ranging from metal springs to elastic bands. Hooke's Law allows us to describe how these springs behave. <router-link to="/hookes-law" class="text-dark"><i class="fas fa-arrow-right"></i></router-link></p>
-    </div>
-  </div>
-
+  
 </template>
