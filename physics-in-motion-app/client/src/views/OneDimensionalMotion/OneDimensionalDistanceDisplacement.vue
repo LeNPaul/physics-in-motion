5c9155495fed5d004818a7eff239797a412b785f
Paul Le <l.nguyen.paul@gmail.com>
2021-12-14 10:53:40 -0500

    #162 Add content for Displacement lesson module

--- physics-in-motion-app/client/src/views/OneDimensionalMotion/OneDimensionalDistanceDisplacement.vue
+++ physics-in-motion-app/client/src/views/OneDimensionalMotion/OneDimensionalDistanceDisplacement.vue
@@ -14,10 +14,19 @@
       <p>If we add the two distances travelled by the particle together from \( t_1 \) to \( t_2 \) and \( t_2 \) to \( t_3 \), we will get the total distance travelled by the particle between \( t_1 \) and \( t_3 \).</p>
       <p>$$ d_3 = d_1 + d_2 $$</p>
 
+    </div>
+    <div class="col-md-6">
+      <div class="d-flex justify-content-center" id="p5Canvas1"></div>
+    </div>
+  </div>
+
+  <div class="row g-5">
+    <div class="col-md-6">
 
       <h3>Displacement</h3>
       <p>Displacement is defined by the distance from the initial position of the particle to the final position. In many cases, this is not the same as the total distance travelled by the particle.</p>
-      <p>For example, consider the case where the particle were to travel from the origin to position \( p_2 \), and then travel back to the origin. Although the total distance travelled was double the distance from the origin to position \( p_2 \) - as the particle had to travel to \( p_2 \) and back from the origin - the total displacement was actually zero, since the particle ended up where it started at the end.</p>
+      <p>Consider the case where the particle were to travel from the origin to position \( p_2 \), and then travel back to the origin. Although the total distance travelled was double the distance from the origin to position \( p_2 \) - as the particle had to travel to \( p_2 \) and back from the origin - the total displacement was actually zero, since the particle ended up where it started at the end.</p>
+      <p>For example, the object on the right starts at \( p_1 = 0 \) and begins moving away from the origin. When it reaches \( p_2 = 3 \), the distance and displacement is 3. However, when it starts moving back towards the origin, the distance continue to increase, while the displacement decreases, until the object reaches the origin again. At that point in time, the distance travelled would be 6 units, while the displacement would be 0 units, since the object is back at the starting position.</p>
 
 
 
@@ -29,11 +38,12 @@
       <p>If we know what \( p(t) \) is as a function, then we know everything we need to know about the motion of the particle, as we will see in later sections.</p>
       <div class="col-sm-2"></div>
 
+    </div>
 
-    </div>
     <div class="col-md-6">
-      <div class="d-flex justify-content-center" id="p5Canvas1"></div>
+      <div class="d-flex justify-content-center" id="p5Canvas2"></div>
     </div>
+
   </div>
 
 </template>
@@ -77,12 +87,11 @@ export default {
           p5.text( 'p3', 285, 275);
 
           p5.textSize(24);
-          p5.text( 'd1', 215, 165);
+          p5.text( 'd1', 215, 90);
           p5.textSize(24);
           p5.text( 'd2', 315, 125);
           p5.textSize(24);
-          p5.text( 'd3', 315, 90);
-
+          p5.text( 'd3', 315, 165);
 
           if(this.x > p5.width - 100) {
             this.x = 0;
@@ -112,13 +121,13 @@ export default {
 
 
         p5.stroke(0, 0, 0);
-        p5.line(0, p5.height * 0.4, 200, p5.height * 0.4);
+        p5.line(0, p5.height * 0.2, 200, p5.height * 0.2);
 
         p5.stroke(0, 0, 0);
         p5.line(200, p5.height * 0.3, 300, p5.height * 0.3);
 
         p5.stroke(0, 0, 0);
-        p5.line(0, p5.height * 0.2, 300, p5.height * 0.2);
+        p5.line(0, p5.height * 0.4, 300, p5.height * 0.4);
 
 
         p5.stroke(0, 0, 0);
@@ -142,6 +151,103 @@ export default {
     }
     const p5canvas1 = new P5(script1, 'p5Canvas1');
 
+    const script2 = p5 => {
+      // this class describes the properties of a single particle.
+      class Particle {
+      // setting the co-ordinates, radius and the
+      // speed of a particle in both the co-ordinates axes.
+        constructor(){
+          this.x = 0;
+          this.y = p5.height / 2;
+          this.r = 20;
+          this.xSpeed = 1;
+          this.ySpeed = 0;
+        }
+
+      // creation of a particle.
+        createParticle() {
+          p5.noStroke();
+          p5.fill('black');
+          p5.circle(this.x,this.y,this.r);
+        }
+
+      // setting the particle in motion.
+        moveParticle() {
+
+          p5.textSize(32);
+          p5.text( 'p1', 0, 275);
+          p5.textSize(32);
+          p5.text( 'p2', 285, 275);
+
+          p5.textSize(24);
+          p5.text( 'd1', 315, 165);
+
+          if(this.xSpeed == 1) {
+            p5.textSize(24);
+            p5.text( 'Distance ' + parseInt(this.x / 100), 0, 120);
+          } else {
+            p5.textSize(24);
+            p5.text( 'Distance ' + parseInt(3+ (3 - this.x / 100)), 0, 120);
+          }
+
+          p5.textSize(24);
+          p5.text( 'Displacement ' + parseInt(this.x / 100), 0, 90);
+
+
+          if(this.x > p5.width - 100) {
+            this.xSpeed *= -1;
+          }
+          if(this.x < 0) {
+            this.xSpeed *= -1;
+          }
+
+          this.x+=this.xSpeed;
+          this.y+=this.ySpeed;
+        }
+
+      }
+
+      // an array to add multiple particles
+      let particles = [];
+
+      p5.setup = () => {
+        p5.createCanvas(400, 400);
+
+        particles.push(new Particle());
+      }
+
+      p5.draw = () => {
+        p5.background('0');
+        for(let i = 0;i<particles.length;i++) {
+          particles[i].createParticle();
+          particles[i].moveParticle();
+        }
+
+
+        p5.stroke(0, 0, 0);
+        p5.line(0, p5.height * 0.4, 300, p5.height * 0.4);
+
+
+        p5.stroke(0, 0, 0);
+        p5.line(0, p5.height * 0.5, p5.width, p5.height * 0.5);
+
+        p5.stroke(0, 0, 0);
+        p5.line(1, 175, 1, 225);
+
+        p5.stroke(0, 0, 0);
+        p5.line(p5.height * 0.25, 175, p5.height * 0.25, 225);
+
+        p5.stroke(0, 0, 0);
+        p5.line(p5.height * 0.5, 175, p5.height * 0.5, 225);
+
+        p5.stroke(0, 0, 0);
+        p5.line(p5.height * 0.75, 175, p5.height * 0.75, 225);
+
+        p5.stroke(0, 0, 0);
+        p5.line(p5.height - 1, 175, p5.height - 1, 225);
+      }
+    }
+    const p5canvas2 = new P5(script2, 'p5Canvas2');
 
   }
 }
