c8b8fcae7e61a89f486ebe7e95659b0eee2b07ce
Paul Le <l.nguyen.paul@gmail.com>
2021-12-19 18:49:42 -0500

    #165 Remove content from Vue.js application to Jekyll site that has been carried over

--- physics-in-motion-app/client/src/views/Home.vue
+++ physics-in-motion-app/client/src/views/Home.vue
@@ -1,16 +1,2 @@
 <template>
-
-  <h1>How physics should be taught.</h1>
-  <p class="fs-5 col-md-8">Not through static diagrams and long blocks of meaningless text, but through dynamic physical simulations that allow you to build an intuition for how physical systems actually work.</p>
-  <div class="mb-5">
-    <router-link to="/" class="btn btn-dark btn-lg px-4">Get Started</router-link>
-  </div>
-
-  <div class="row g-5 mb-5">
-    <div class="col-md-6">
-      <h2>Motion in One Dimension</h2>
-      <p>Start with the most basic example of physics in motion: motion in one dimension. Learn about position, velocity, and acceleration. <router-link to="/one-dimensional-motion" class="text-dark"><i class="fas fa-arrow-right"></i></router-link></p>
-    </div>
-  </div>
-  
 </template>
--- physics-in-motion-app/client/src/views/OneDimensionalMotion.vue
+++ physics-in-motion-app/client/src/views/OneDimensionalMotion.vue
@@ -1,26 +1,2 @@
 <template>
-  <h1>Motion in One Dimension</h1>
-  <p class="fs-5 col-md-8 mb-5">Start with the most basic example of physics in motion: motion in one dimension. Learn about position, velocity, and acceleration.</p>
-
-  <div class="row g-5 mb-3">
-    <div class="col-md-6">
-      <h2>Position</h2>
-      <p>Start with the most basic example of physics in motion: position in one dimension. Learn about how we describe the position of an object. <router-link to="/one-dimensional-position" class="text-dark"><i class="fas fa-arrow-right"></i></router-link></p>
-    </div>
-  </div>
-
-  <div class="row g-5 mb-3">
-    <div class="col-md-6">
-      <h2>Distance and Displacement</h2>
-      <p>How do we describe an object that has moved? Learn about distance, displacement, and the difference between the two. <router-link to="/one-dimensional-distance-displacement" class="text-dark"><i class="fas fa-arrow-right"></i></router-link></p>
-    </div>
-  </div>
-
-  <div class="row g-5 mb-3">
-    <div class="col-md-6">
-      <h2>Speed and Velocity</h2>
-      <p>Not all distance and displacement is the same. Learn about speed, velocity, and the difference between the two. <router-link to="/one-dimensional-speed-velocity" class="text-dark"><i class="fas fa-arrow-right"></i></router-link></p>
-    </div>
-  </div>
-
 </template>
